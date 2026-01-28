29c815d9f678b997ab939dc942e80e8861483f3a
danielleblanc662-code <danielleblanc662@gmail.com>
2026-01-27 21:17:24 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // This is the fix! It tells GitHub to look in the /Prism/ folder
     base: '/Prism/', 
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
     plugins: [react()],
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-    },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
